1ec61544fe2cb89692f80b7d945f82b14977cf64
Chaithu anv <60737573+Chaithu5@users.noreply.github.com>
2021-10-05 14:34:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Speech-to-Text
+# Basic Speech-To-Text Convertor using Google API
+
+A Python script to covert speech from microphone to text and display on the  screen.
+
+# Libraries
+
+-Python3
+ -An IDE
+ -Speech Recognition Module
+ -PyAudio Module
+ -PiP Installer
+
+```pip install SpeechRecognition```
+```pip install PyAudio```
+
+
---
+++ speechTotext.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak into mic: ")
+    audio = r.listen(source)
+
+try:
+    print("You said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Didn't understand what you said!")
+except sr.RequestError as e:
+    print("Cannot obtain results; {0}".format(e))
